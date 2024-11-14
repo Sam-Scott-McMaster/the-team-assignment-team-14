7792d7c1e155fefb8826af933ba13eab6e34e279
tharnyelilvannan <tharny2005@outlook.com>
2024-11-13 21:46:05 -0500

    Draw border and corkboard

--- main.c
+++ main.c
@@ -11,21 +11,53 @@
 // #include <SDL3/SDL_image.h>
 #include "secrets.h"
 
-// exit code 1: error while initializing
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
 
 int main(int argc, char *argv[]) {
 
-    if (SDL_Init(SDL_INIT_EVENTS) == 1) {
+    if (SDL_Init(SDL_INIT_EVENTS) == -1) {
 
         fprintf(stderr, "%s\n", SDL_GetError());
-        exit(1);
+        exit(EXIT_FAILURE);
 
     } // end of if statement
 
-    SDL_Window* window = SDL_CreateWindow("SECRETS OF SUMMERSIDE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, SDL_WINDOW_FULLSCREEN);
+    SDL_Window* window = SDL_CreateWindow("SECRETS OF SUMMERSIDE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 2000, 1000, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (renderer == NULL) {
+
+        fprintf(stderr, "%s", SDL_GetError());
+        exit(EXIT_FAILURE);
+
+    } // end of if statement
+
+    SDL_Rect border = {0, 0, 2000, 1000};
+    SDL_Rect corkboard = {50, 50, 1900, 900};
 
     while (true) {
 
+        SDL_SetWindowResizable(window, SDL_FALSE);
+        SDL_SetWindowKeyboardGrab(window, SDL_TRUE);
+        SDL_SetWindowMouseGrab(window, SDL_TRUE);
+        SDL_SetWindowGrab(window, SDL_TRUE);
+        
+        // draw border
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(renderer, &border);
+
+        // draw corkboard
+        SDL_SetRenderDrawColor(renderer, 189, 154, 122, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(renderer, &corkboard);
+
+        // display
+        SDL_RenderPresent(renderer);
+        SDL_Delay(5000);
+
     } // end of while loop
+
+    SDL_Quit;
+    exit(EXIT_SUCCESS);
 
 } // end of main function
