205bed638b38ad72ca505108d99b8fac8e25b59a
megs <rogerm21@mcmaster.ca>
2024-11-24 21:39:08 -0500

    new make command added, headers added for recipe.c

--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 secrets: main.c secrets.h
 	gcc -o secrets main.c -lSDL2 -lSDL2_image
+
+recipe: recipe.c
+	gcc -o recipe recipe.c `sdl2-config --libs --cflags` -ggdb3 -O0 --std=c99 -Wall -F/Library/Frameworks -framework SDL2 -framework SDL2_image -framework SDL2_ttf -lm
--- recipe.c
+++ recipe.c
@@ -1,6 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* Meigan Rogers, Nov 24 2024
+ * 
+ * This function will "enlarge" one of the clues that the user clicks on
+ * Focuses on the recipe clue
+ * 
+ * McMaster University
+ * 
+ * */
 
-int main(int argc, char *argv[]){
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#ifdef __APPLE_CC__
+#include <SDL2/SDL.h>
+#include <SDL_timer.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#else
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#endif
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+int main(int argc, char *argv[]) {
     puts("hi"); 
-}
+
+} // end of main function
